a6d9760d9393c081bdb0c7c269bda6e0f9aa781c
IYamiSukiI <rooboot7898@gmail.com>
2026-09-04 08:09:33 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,39 +2,3 @@ plugins {
 id("com.android.application")
 id("org.jetbrains.kotlin.android")
 }
-
-android {
-namespace = "com.sabziyar.app"
-compileSdk = 34
-
-defaultConfig {
-applicationId = "com.sabziyar.app"
-minSdk = 21
-targetSdk = 34
-versionCode = 1
-versionName = "1.0"
-}
-
-compileOptions {
-sourceCompatibility = JavaVersion.VERSION_17
-targetCompatibility = JavaVersion.VERSION_17
-}
-
-kotlinOptions {
-jvmTarget = "17"
-}
-
-buildTypes {
-release {
-isMinifyEnabled = false
-}
-debug {
-isMinifyEnabled = false
-}
-}
-}
-
-dependencies {
-implementation("androidx.core:core-ktx:1.12.0")
-implementation("androidx.appcompat:appcompat:1.6.1")
-}
